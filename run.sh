4c474f7c67268d47b910803b63e697249dfa8454
Lynn <lynn@foldr.moe>
2023-06-27 00:19:30 +0200

    --cli flag for run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-./compile.sh $1 > tmp.tal && uxnasm tmp.tal tmp.rom && uxnemu tmp.rom
+emu=uxnemu
+if [ "$1" == "--cli" ]; then emu=uxncli; shift; fi
+./compile.sh $1 > tmp.tal && uxnasm tmp.tal tmp.rom && $emu tmp.rom
