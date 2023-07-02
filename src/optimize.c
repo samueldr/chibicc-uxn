6caabb6a76e22f4a107a262e02776ac2ce540295
Lynn <lynn@foldr.moe>
2023-07-02 11:38:11 +0200

    IsLit, IsLit2 macros + clang-format optimize.c

--- src/optimize.c
+++ src/optimize.c
@@ -1,15 +1,14 @@
 #include "chibi.h"
 
 char *opcode_names[0x20] = {
-  "BRK", "INC", "POP", "NIP", "SWP", "ROT", "DUP", "OVR",
-  "EQU", "NEQ", "GTH", "LTH", "JMP", "JCN", "JSR", "STH",
-  "LDZ", "STZ", "LDR", "STR", "LDA", "STA", "DEI", "DEO",
-  "ADD", "SUB", "MUL", "DIV", "AND", "ORA", "EOR", "SFT",
+  "BRK", "INC", "POP", "NIP", "SWP", "ROT", "DUP", "OVR", "EQU", "NEQ", "GTH",
+  "LTH", "JMP", "JCN", "JSR", "STH", "LDZ", "STZ", "LDR", "STR", "LDA", "STA",
+  "DEI", "DEO", "ADD", "SUB", "MUL", "DIV", "AND", "ORA", "EOR", "SFT",
 };
 
 Instruction *emit_head;
 
-void emit(Opcode opcode, int literal, char* label) {
+void emit(Opcode opcode, int literal, char *label) {
   emit_head->opcode = opcode;
   emit_head->literal = literal;
   if (label)
@@ -20,32 +19,67 @@ void emit(Opcode opcode, int literal, char* label) {
 
 char buf[4096];
 void op(Opcode o) { emit(o, 0, ""); }
-void jci(char* fmt, ...) { va_list va; va_start(va, fmt); vsprintf(buf, fmt, va); va_end(va); emit(JCI, 0, buf); }
-void jmi(char* fmt, ...) { va_list va; va_start(va, fmt); vsprintf(buf, fmt, va); va_end(va); emit(JMI, 0, buf); }
-void jsi(char* fmt, ...) { va_list va; va_start(va, fmt); vsprintf(buf, fmt, va); va_end(va); emit(JSI, 0, buf); }
+void jci(char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  vsprintf(buf, fmt, va);
+  va_end(va);
+  emit(JCI, 0, buf);
+}
+void jmi(char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  vsprintf(buf, fmt, va);
+  va_end(va);
+  emit(JMI, 0, buf);
+}
+void jsi(char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  vsprintf(buf, fmt, va);
+  va_end(va);
+  emit(JSI, 0, buf);
+}
 void lit(unsigned char n) { emit(LIT, n, ""); }
 void lit2(unsigned short n) { emit(LIT2, n, ""); }
 void lit2r(unsigned short n) { emit(LIT2r, n, ""); }
-void at(char* fmt, ...) { va_list va; va_start(va, fmt); vsprintf(buf, fmt, va); va_end(va); emit(AT, 0, buf); }
-void semi(char* fmt, ...) { va_list va; va_start(va, fmt); vsprintf(buf, fmt, va); va_end(va); emit(SEMI, 0, buf); }
+void at(char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  vsprintf(buf, fmt, va);
+  va_end(va);
+  emit(AT, 0, buf);
+}
+void semi(char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  vsprintf(buf, fmt, va);
+  va_end(va);
+  emit(SEMI, 0, buf);
+}
 void bar(unsigned short n) { emit(BAR, n, ""); }
+#define IsLit(p, n) ((p) && (p)->opcode == LIT && (p)->literal == n)
+#define IsLit2(p, n) ((p) && (p)->opcode == LIT2 && (p)->literal == n)
 
-#define ConstantFolding(target_opcode, c_operator, result_opcode) \
-    if (prog->opcode == LIT2 && prog->next->opcode == LIT2 && prog->next->next->opcode == (target_opcode | flag_2)) { \
-      prog->opcode = result_opcode; \
-      prog->literal = prog->literal c_operator prog->next->literal; \
-      prog->next = prog->next->next->next; \
-      changed = true; \
-      continue; \
-    } \
-    if (prog->opcode == LIT && prog->next->opcode == LIT && prog->next->next->opcode == target_opcode) { \
-      prog->literal = (unsigned char)prog->literal c_operator (unsigned char)prog->next->literal; \
-      prog->next = prog->next->next->next; \
-      changed = true; \
-      continue; \
-    }
+#define ConstantFolding(target_opcode, c_operator, result_opcode)              \
+  if (prog->opcode == LIT2 && prog->next->opcode == LIT2 &&                    \
+      prog->next->next->opcode == (target_opcode | flag_2)) {                  \
+    prog->opcode = result_opcode;                                              \
+    prog->literal = prog->literal c_operator prog->next->literal;              \
+    prog->next = prog->next->next->next;                                       \
+    changed = true;                                                            \
+    continue;                                                                  \
+  }                                                                            \
+  if (prog->opcode == LIT && prog->next->opcode == LIT &&                      \
+      prog->next->next->opcode == target_opcode) {                             \
+    prog->literal = (unsigned char)prog->literal c_operator(unsigned char)     \
+                        prog->next->literal;                                   \
+    prog->next = prog->next->next->next;                                       \
+    changed = true;                                                            \
+    continue;                                                                  \
+  }
 
-static bool optimize_pass(Instruction* prog, int stage) {
+static bool optimize_pass(Instruction *prog, int stage) {
   bool changed = false;
   while (prog->opcode) {
     if (prog->next && prog->next->next) {
@@ -71,7 +105,7 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // #0001 ADD2 -> INC2
-    if (prog->opcode == LIT2 && prog->literal == 1 && prog->next && prog->next->opcode == ADD2) {
+    if (IsLit2(prog, 1) && prog->next && prog->next->opcode == ADD2) {
       prog->opcode = INC2;
       prog->next = prog->next->next;
       changed = true;
@@ -79,7 +113,9 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // #0000 +-|^ -> nothing
-    if (prog->opcode == LIT2 && prog->literal == 0 && prog->next && (prog->next->opcode == ADD2 || prog->next->opcode == SUB2 || prog->next->opcode == ORA2 || prog->next->opcode == EOR2)) {
+    if (IsLit2(prog, 0) && prog->next &&
+        (prog->next->opcode == ADD2 || prog->next->opcode == SUB2 ||
+         prog->next->opcode == ORA2 || prog->next->opcode == EOR2)) {
       memcpy(prog, prog->next->next, sizeof(Instruction));
       changed = true;
       continue;
@@ -93,31 +129,37 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // sext NIP cancels out
-    if (prog->opcode == JSI && !strcmp("sext", prog->label) && prog->next && prog->next->opcode == NIP) {
+    if (prog->opcode == JSI && !strcmp("sext", prog->label) && prog->next &&
+        prog->next->opcode == NIP) {
       memcpy(prog, prog->next->next, sizeof(Instruction));
       changed = true;
       continue;
     }
 
     // LIT SWP NIP cancels out
-    if (prog->opcode == LIT && prog->next && prog->next->opcode == SWP && prog->next->next && prog->next->next->opcode == NIP) {
+    if (prog->opcode == LIT && prog->next && prog->next->opcode == SWP &&
+        prog->next->next && prog->next->next->opcode == NIP) {
       memcpy(prog, prog->next->next->next, sizeof(Instruction));
       changed = true;
       continue;
     }
 
     // #0001 muldiv -> nothing
-    if (prog->opcode == LIT2 && prog->literal == 1 && prog->next && (prog->next->opcode == MUL2 || prog->next->opcode == DIV2)) {
+    if (IsLit2(prog, 1) && prog->next &&
+        (prog->next->opcode == MUL2 || prog->next->opcode == DIV2)) {
       memcpy(prog, prog->next->next, sizeof(Instruction));
       changed = true;
       continue;
     }
 
     // pow2 muldiv -> SFT2
-    if (prog->opcode == LIT2 && prog->literal > 1 && (prog->literal & (prog->literal - 1)) == 0 && prog->next && (prog->next->opcode == MUL2 || prog->next->opcode == DIV2)) {
+    if (prog->opcode == LIT2 && prog->literal > 1 &&
+        (prog->literal & (prog->literal - 1)) == 0 && prog->next &&
+        (prog->next->opcode == MUL2 || prog->next->opcode == DIV2)) {
       prog->opcode = LIT;
       int log2 = 0;
-      while (prog->literal >>= 1) log2++;
+      while (prog->literal >>= 1)
+        log2++;
       prog->literal = log2 << (prog->next->opcode == MUL2 ? 4 : 0);
       prog->next->opcode = SFT2;
       changed = true;
@@ -134,7 +176,8 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // Combine literals
-    if (stage >= 1 && prog->opcode == LIT && prog->next && prog->next->opcode == LIT) {
+    if (stage >= 1 && prog->opcode == LIT && prog->next &&
+        prog->next->opcode == LIT) {
       prog->opcode = LIT2;
       prog->literal = prog->literal << 8 | prog->next->literal;
       prog->next = prog->next->next;
@@ -143,10 +186,9 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // Associative addition: x ADD y ADD = (x+y) ADD
-    if (prog->opcode == LIT2
-        && prog->next && prog->next->opcode == ADD2
-        && prog->next->next && prog->next->next->opcode == LIT2
-        && prog->next->next->next && prog->next->next->next->opcode == ADD2) {
+    if (prog->opcode == LIT2 && prog->next && prog->next->opcode == ADD2 &&
+        prog->next->next && prog->next->next->opcode == LIT2 &&
+        prog->next->next->next && prog->next->next->next->opcode == ADD2) {
       prog->literal += prog->next->next->literal;
       prog->next->next = prog->next->next->next->next;
       changed = true;
@@ -154,9 +196,8 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // A common sequence: STA2k POP2 POP2 -> STA2
-    if (prog->opcode == STA2k
-        && prog->next && prog->next->opcode == POP2
-        && prog->next->next && prog->next->next->opcode == POP2) {
+    if (prog->opcode == STA2k && prog->next && prog->next->opcode == POP2 &&
+        prog->next->next && prog->next->next->opcode == POP2) {
       prog->opcode = STA2;
       prog->next = prog->next->next->next;
       changed = true;
@@ -164,9 +205,9 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // A common sequence: STAk POP2 POP2 -> STA POP
-    if (prog->opcode == (STA | flag_k)
-        && prog->next && prog->next->opcode == POP2
-        && prog->next->next && prog->next->next->opcode == POP2) {
+    if (prog->opcode == (STA | flag_k) && prog->next &&
+        prog->next->opcode == POP2 && prog->next->next &&
+        prog->next->next->opcode == POP2) {
       prog->opcode = STA;
       prog->next->opcode = POP;
       prog->next->next = prog->next->next->next;
@@ -175,19 +216,22 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // DUP2 (INC2|LDA2|DUP2) -> +k
-    if (prog->opcode == DUP2 && prog->next && (prog->next->opcode == INC2 || prog->next->opcode == LDA2 || prog->next->opcode == DUP2)) {
+    if (prog->opcode == DUP2 && prog->next &&
+        (prog->next->opcode == INC2 || prog->next->opcode == LDA2 ||
+         prog->next->opcode == DUP2)) {
       prog->opcode = (prog->next->opcode | flag_k);
       prog->next = prog->next->next;
       changed = true;
       continue;
     }
 
-    // A common sequence: STH2kr #xxxx ADD2 #yyyy SWP2 -> #yyyy STH2kr #xxxx ADD2
-    if (prog->opcode == STH2kr
-        && prog->next && prog->next->opcode == LIT2
-        && prog->next->next && prog->next->next->opcode == ADD2
-        && prog->next->next->next && prog->next->next->next->opcode == LIT2
-        && prog->next->next->next->next && prog->next->next->next->next->opcode == SWP2) {
+    // A common sequence: STH2kr #xxxx ADD2 #yyyy SWP2 -> #yyyy STH2kr #xxxx
+    // ADD2
+    if (prog->opcode == STH2kr && prog->next && prog->next->opcode == LIT2 &&
+        prog->next->next && prog->next->next->opcode == ADD2 &&
+        prog->next->next->next && prog->next->next->next->opcode == LIT2 &&
+        prog->next->next->next->next &&
+        prog->next->next->next->next->opcode == SWP2) {
       int x = prog->next->literal;
       prog->opcode = LIT2;
       prog->literal = prog->next->next->next->literal;
@@ -201,11 +245,10 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // EQU2 #00 SWP #0000 EQU2 -> NEQ2
-    if (prog->opcode == EQU2
-        && prog->next && prog->next->opcode == LIT && prog->next->literal == 0
-        && prog->next->next && prog->next->next->opcode == SWP
-        && prog->next->next->next && prog->next->next->next->opcode == LIT2 && prog->next->next->next->literal == 0
-        && prog->next->next->next->next && prog->next->next->next->next->opcode == EQU2) {
+    if (prog->opcode == EQU2 && IsLit(prog->next, 0) && prog->next->next &&
+        prog->next->next->opcode == SWP && IsLit2(prog->next->next->next, 0) &&
+        prog->next->next->next->next &&
+        prog->next->next->next->next->opcode == EQU2) {
       prog->opcode = NEQ2;
       prog->next = prog->next->next->next->next->next;
       changed = true;
@@ -213,11 +256,10 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // #00 SWP #0000 EQU2 ? -> #00 EQU ?
-    if (prog->opcode == LIT && prog->literal == 0
-        && prog->next && prog->next->opcode == SWP
-        && prog->next->next && prog->next->next->opcode == LIT2 && prog->next->next->literal == 0
-        && prog->next->next->next && prog->next->next->next->opcode == EQU2
-        && prog->next->next->next->next->opcode == JCI) {
+    if (IsLit(prog, 0) && prog->next && prog->next->opcode == SWP &&
+        IsLit2(prog->next->next, 0) && prog->next->next->next &&
+        prog->next->next->next->opcode == EQU2 &&
+        prog->next->next->next->next->opcode == JCI) {
       prog->next->opcode = EQU;
       prog->next->next = prog->next->next->next->next;
       changed = true;
@@ -225,28 +267,27 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // sext #0000 NEQ2 ? -> ?
-    if (prog->opcode == JSI && !strcmp("sext", prog->label)
-        && prog->next && prog->next->opcode == LIT2 && prog->next->literal == 0
-        && prog->next->next && prog->next->next->opcode == NEQ2
-        && prog->next->next->next->opcode == JCI) {
+    if (prog->opcode == JSI && !strcmp("sext", prog->label) &&
+        IsLit2(prog->next, 0) && prog->next->next &&
+        prog->next->next->opcode == NEQ2 &&
+        prog->next->next->next->opcode == JCI) {
       memcpy(prog, prog->next->next->next, sizeof(Instruction));
       changed = true;
       continue;
-    // same with zext (#00 SWP)
-    } else if (prog->opcode == LIT && prog->literal == 0
-        && prog->next && prog->next->opcode == SWP
-        && prog->next->next && prog->next->next->opcode == LIT2 && prog->next->next->literal == 0
-        && prog->next->next->next && prog->next->next->next->opcode == NEQ2
-        && prog->next->next->next->next->opcode == JCI) {
+      // same with zext (#00 SWP)
+    } else if (prog->opcode == LIT && prog->literal == 0 && prog->next &&
+               prog->next->opcode == SWP && IsLit2(prog->next->next, 0) &&
+               prog->next->next->next &&
+               prog->next->next->next->opcode == NEQ2 &&
+               prog->next->next->next->next->opcode == JCI) {
       memcpy(prog, prog->next->next->next->next, sizeof(Instruction));
       changed = true;
       continue;
     }
 
     // #0000 NEQ2 ? -> ORA ?
-    if (prog->opcode == LIT2 && prog->literal == 0
-        && prog->next && prog->next->opcode == NEQ2
-        && prog->next->next && prog->next->next->opcode == JCI) {
+    if (IsLit2(prog, 0) && prog->next && prog->next->opcode == NEQ2 &&
+        prog->next->next && prog->next->next->opcode == JCI) {
       prog->opcode = ORA;
       prog->next = prog->next->next;
       changed = true;
@@ -254,9 +295,9 @@ static bool optimize_pass(Instruction* prog, int stage) {
     }
 
     // sext #00xx AND2 -> #xx AND #00 SWP
-    if (prog->opcode == JSI && !strcmp("sext", prog->label)
-        && prog->next && prog->next->opcode == LIT2 && !(prog->next->literal & 0xff00)
-        && prog->next->next && prog->next->next->opcode == AND2) {
+    if (prog->opcode == JSI && !strcmp("sext", prog->label) && prog->next &&
+        prog->next->opcode == LIT2 && !(prog->next->literal & 0xff00) &&
+        prog->next->next && prog->next->next->opcode == AND2) {
       prog->opcode = LIT;
       prog->literal = prog->next->literal & 0xff;
       prog->next->opcode = AND;
@@ -268,11 +309,12 @@ static bool optimize_pass(Instruction* prog, int stage) {
       prog->next->next->next = new;
       changed = true;
       continue;
-    // ditto with zext (#00 SWP)
-    } else if (prog->opcode == LIT && prog->literal == 0
-        && prog->next && prog->next->opcode == SWP
-        && prog->next->next && prog->next->next->opcode == LIT2 && !(prog->next->next->literal & 0xff00)
-        && prog->next->next->next && prog->next->next->next->opcode == AND2) {
+      // ditto with zext (#00 SWP)
+    } else if (IsLit(prog, 0) && prog->next && prog->next->opcode == SWP &&
+               prog->next->next && prog->next->next->opcode == LIT2 &&
+               !(prog->next->next->literal & 0xff00) &&
+               prog->next->next->next &&
+               prog->next->next->next->opcode == AND2) {
       prog->opcode = LIT;
       prog->literal = prog->next->next->literal & 0xff;
       prog->next->opcode = AND;
@@ -288,12 +330,14 @@ static bool optimize_pass(Instruction* prog, int stage) {
   return changed;
 }
 
-void optimize(Instruction* prog) {
-  while (optimize_pass(prog, 0));
-  while (optimize_pass(prog, 1));
+void optimize(Instruction *prog) {
+  while (optimize_pass(prog, 0))
+    ;
+  while (optimize_pass(prog, 1))
+    ;
 }
 
-void output_one(Instruction* ins) {
+void output_one(Instruction *ins) {
   if (ins->opcode == AT) {
     printf("%s%s", ins->label[0] == '&' ? "" : "@", ins->label);
   } else if (ins->opcode == BAR) {
@@ -316,17 +360,16 @@ void output_one(Instruction* ins) {
     printf("LIT2r %04x", ins->literal);
   } else {
     printf("%s%s%s%s", opcode_names[ins->opcode & 0x1f],
-      ins->opcode & flag_2 ? "2" : "",
-      ins->opcode & flag_k ? "k" : "",
-      ins->opcode & flag_r ? "r" : "");
+           ins->opcode & flag_2 ? "2" : "", ins->opcode & flag_k ? "k" : "",
+           ins->opcode & flag_r ? "r" : "");
   }
 }
 
-void output(Instruction* prog) {
+void output(Instruction *prog) {
   bool new_line = true; // did we just have a \n and need to re-indent?
   while (prog->opcode) {
     if (prog->opcode == AT || prog->opcode == BAR) {
-      printf("\n"); // double newline for all labels
+      printf("\n");  // double newline for all labels
       if (!new_line) // avoid triple newline after certain instructions
         printf("\n");
       if (prog->opcode == AT && prog->label[0] == '&')
